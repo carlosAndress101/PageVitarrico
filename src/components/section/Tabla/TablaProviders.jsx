d5ac2c43b63ef3b8c0657083008cae9d6f57ab96
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-06-13 18:42:47 -0500

    Deploy

---
+++ src/components/section/Tabla/TablaProviders.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './TablaOrders.css';
+
+const TablaProviders = ({customs}) => {
+  return (
+    <>
+    <table className="content-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>PHONENUMBER</th>
+          <th>CITY</th>
+          <th>DIRECTION</th>
+        </tr>
+      </thead>
+      <tbody>
+        {customs.map((p) => (
+          <tr key={p._id} className="active-row">
+            <td>{p._id}</td>
+            <td>{p.Name}</td>    
+            <td>{p.Phonenumber}</td>    
+            <td>{p.city}</td>    
+            <td>{p.Direction}</td>    
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </>
+  )
+}
+
+export default TablaProviders
